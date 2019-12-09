0be4d6a04493571b8e833c8bf1de42dc9fa1196f
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-12-08 23:58:05 -0300

    adicionado doi fragments para exibição

--- app/src/main/java/br/com/barrsoft/smarttablayout/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/smarttablayout/MainActivity.java
@@ -1,14 +1,41 @@
 package br.com.barrsoft.smarttablayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+    @BindView(R.id.viewpagertab)
+    SmartTabLayout viewPagerTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ButterKnife.bind(this);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.titleA, PageFragment.class)
+                .add(R.string.titleB, PageFragment.class)
+                .create());
+
+        viewPager.setAdapter(adapter);
+        viewPagerTab.setViewPager(viewPager);
+
+
     }
 }
